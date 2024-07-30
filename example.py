5ae3e4fc189d7fcc75a45811e24984b208e639db
kadir-zeren <k4dzeren@gmail.com>
2024-07-30 14:24:16 +0300

    python example

--- example.py
+++ example.py
@@ -1,1 +1,52 @@
 print('Hello world')
+print('Benim yazdigim ilk kod')
+print("Cift tirnak yazdim")
+print('''uc tirnak ile yazdim''')
+print("bir cumle")
+print("I'm great")
+print('"hi" he said')
+print('''Don't do it" said the manager''')
+print('ben bunu yazmak istiyorum')
+print('''bu birinci satir
+bu ikinci satir
+bu uc
+bu dort''')
+print('''bu birinci 'satir
+bu ikinci satir"
+bu uc
+bu dort''')
+print('3.14')
+print(3.14)
+print('3.14' *3)
+print(3.14*3)
+print('abcd' *2)
+print(5*2)
+print(''''We should have enough time for our family"''')
+print('first line')
+print()
+print('''third line''')
+print('bir')
+print()
+print('uc')
+print('first line')
+print()
+print('''third line''')
+print()
+print('')
+print()
+print("")
+print('I\'m great')
+print("\"don't do it\" said the manager")
+print( 'bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print("ben kodlarkjlj vl k k k kk 11 1kjlkjlkjljlk \
+kdkdkdkdkdkd\
+kdkdkdkdkd")
+# buradaki fonksiyon kare alma fonksiyonu
+print('kullanici ismi') # Burada kullanici ismi yazddiriliyor.
+x = 5 # burada x degeri atandi
+print( 'hello') # This is an inline comment
+print ('hello')
+# First multi-line comment
+# Second multi-line comment
+# Third multi-line comment
